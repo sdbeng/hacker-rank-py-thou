5fec5eab805c482cccf2a73c029e0a65cf1aa022
Sergio <sdbeng@gmail.com>
2019-04-26 22:35:43 -0700

    Update fibonacci_end.py

--- fibonacci_end.py
+++ fibonacci_end.py
@@ -26,3 +26,11 @@
 cat 3
 window 6
 defenestrate 12
+
+# If you need to modify the sequence you are iterating over while inside the loop (for example to duplicate selected items), it is recommended that you first make a copy. Iterating over a sequence does not implicitly make a copy. The slice notation makes this especially convenient:
+>>> for w in words[:]:  # Loop over a slice copy of the entire list.
+...     if len(w) > 6:
+...         words.insert(0, w)
+...
+>>> words
+['defenestrate', 'cat', 'window', 'defenestrate']
